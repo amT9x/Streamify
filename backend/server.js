8ced10e36d353cde8bf14d67cbc081dbe6fe1a83
amT9x <hoangvantung155@gmail.com>
2025-05-14 08:38:08 +0700

    be: create .env file &sStandardize variabl: PORT

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
