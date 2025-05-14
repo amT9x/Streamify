4f14317402db3ac01582d2fdf177adac99d5156b
amT9x <hoangvantung155@gmail.com>
2025-05-14 16:42:30 +0700

    be:clean code create folder & routes file use Express Router

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,12 @@
 import express from 'express';
 import "dotenv/config";
+import authRoutes from './routes/auth.route.js';
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/api/auth/signup', (req, res) => {
-    res.send('SignUp Route!');
-})
-
-app.get('/api/auth/login', (req, res) => {
-    res.send('Login Route!');
-})
-
-app.get('/api/auth/logout', (req, res) => {
-    res.send('LogOut Route!');
-})
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);   
 });
