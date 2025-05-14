06a53dc9489fd6b59efef5574d2f89df8d22fb6a
amT9x <hoangvantung155@gmail.com>
2025-05-14 21:04:45 +0700

    be: loi tao use & import sai

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from '../models/User.js';
+import User from '../modules/User.js';
 import jwt from 'jsonwebtoken';
 
 export async function signup(req, res) {
@@ -28,7 +28,7 @@ export async function signup(req, res) {
 
         const idx = Math.floor(Math.random() * 100) + 1;
         const ramdomAvartar = `https://avatar.iran.liara.run/public/${idx}.png`;
-        const newUser = new User({
+        const newUser = new User.create({
             fullName,
             email,
             password,
