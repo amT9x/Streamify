5a980892cf4ed41dd9c1323fbdb64e9812f61d52
amT9x <hoangvantung155@gmail.com>
2025-05-14 17:44:21 +0700

    be: bam mat khau cung voi Salt bcrypt

--- backend/src/modules/User.js
+++ backend/src/modules/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     fullName: {
@@ -49,5 +50,15 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
+
+userSchema.pre("save", async function (next) {
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 export default User;
