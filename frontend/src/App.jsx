836747bc4e7f795dbbb64c8a703a5bbcdf7af602
amT9x <hoangvantung155@gmail.com>
2025-05-15 13:35:13 +0700

    fe: init & config route pages: home, signup, login, onboarding, chat, call, notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import OnBoardingPage from './pages/OnBoardingPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import NotificationsPage from './pages/NotificationsPage.jsx'
 
 const App = () => {
   return (
     <div className= 'h-screen' data-theme="night">
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnBoardingPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+      </Routes>
     </div>
   )
 }
