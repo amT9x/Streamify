6ea6ff099279efec8892c0c3c8a67f14dee60422
amT9x <hoangvantung155@gmail.com>
2025-05-24 21:00:33 +0700

    fe: update ui logo Streamify

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {ShipWheelIcon} from 'lucide-react'
 
 const SignUpPage = () => {
+
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSignup = (eventt) => {
+    eventt.preventDefault();
+  }
+
   return (
-    <div>SignUpPage</div>
+    <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="forest">
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+      {/* Sign up form - left side */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* Logo */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-2xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wide">
+            Streamify
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
