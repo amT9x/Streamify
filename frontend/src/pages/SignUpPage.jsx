5bad1d66741afe895139a13c1d9e20936ebf8961
amT9x <hoangvantung155@gmail.com>
2025-05-24 21:30:33 +0700

    fe: update ui: signup form - left side

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {ShipWheelIcon} from 'lucide-react'
+import { Link } from 'react-router';
 
 const SignUpPage = () => {
 
@@ -24,6 +25,93 @@ const SignUpPage = () => {
             <span className="text-2xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wide">
             Streamify
             </span>
+          </div>
+          {/* Form */}
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
+                <div>
+                  <h2 classname="text-xl font-semibold">Create an account</h2>
+                  <p className="text-sm opacity-70">
+                    Join Streamify and start your languagelearning adventure!
+                  </p>
+                </div>
+                {/* Fullname */}
+                <div className="space-y-3">
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Full Name</span>
+                    </label>
+
+                    <input
+                      type="text"
+                      placeholder="TungHV"
+                      className="input input-bordered w-full"
+                      value={signupData.fullName}
+                      onChange={(e) => setSignupData({...signupData, fullName: e.target.value})}
+                      required
+                      />
+                  </div>
+                  {/* Email */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+
+                    <input
+                      type="email"
+                      placeholder="...@email.com"
+                      className="input input-bordered w-full"
+                      value={signupData.email}
+                      onChange={(e) => setSignupData({...signupData, email: e.target.value})}
+                      required
+                      />
+                  </div>
+                  {/* Password */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+
+                    <input
+                      type="password"
+                      placeholder="********"
+                      className="input input-bordered w-full"
+                      value={signupData.password}
+                      onChange={(e) => setSignupData({...signupData, password: e.target.value})}
+                      required
+                      />
+                    <div className="text-xs opacity-70 mt-1">
+                    Passord must be at least 6 characters 
+                    </div>
+                  </div>
+                  {/* Checkbox accept */}
+                  <div className="form-control">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input type="checkbox" className="checkbox checkbox-sm"/>
+                      <span className="text-xs leading-tight">
+                        I agree to the {" "}
+                        <span className="text-primary hover:underline">term of services</span> and {" "}
+                        <span className="text-primary hover:underline">privacy policy</span>
+                      </span>
+                    </label>
+                  </div>
+                  {/* Submit sign up */}
+                  <button className="btn btn-primary w-full" type="submit">
+                    Create Account
+                  </button>
+                  {/* Sign in */}
+                  <div className="text-center mt-4">
+                    <p className="text-sm">
+                      Alreadly have an account? {" "}
+                      <Link to="/login" className="text-primary hover:underline"> 
+                        Sign In
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
