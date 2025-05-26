f659684ae49fdc83eb9fa62d6b106961155d95af
amT9x <hoangvantung155@gmail.com>
2025-05-27 00:27:25 +0700

    fe: logic gui thong tin onboarding len server

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -9,3 +9,8 @@ export const getAuthUser = async () => {
     const res = await axiosInstance.get('http://localhost:5001/api/auth/me');
     return res.data;
 }
+
+export const completeOnboarding = async (userData) => {
+    const res = await axiosInstance.post('http://localhost:5001/api/auth/onboarding', userData);
+    return res.data;
+}
--- frontend/src/pages/OnBoardingPage.jsx
+++ frontend/src/pages/OnBoardingPage.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useAuthUser from '../hooks/useAuthUser'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { completeOnboarding } from '../lib/api';
 
 const OnBoardingPage = () => {
+  const {authUser} = useAuthUser();
+  const queryClient = useQueryClient();
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePicture: authUser?.profilePicture || "",
+  });
+
+  const {mutate: onboardingMutation, isPending} = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () => {
+      toast.success("Profile onboarding success!");
+      queryClient.invalidateQueries({queryKey: ['authUser']});
+    }
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // ngăn trình duyệt reload lại trang mặc định khi submit form
+    onboardingMutation(formState);
+  }
+
   return (
     <div>OnBoardingPage</div>
   )
