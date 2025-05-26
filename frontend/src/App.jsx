f2a14826ab9cfe5e7b800dea3633fa52b3d0679c
amT9x <hoangvantung155@gmail.com>
2025-05-26 21:34:41 +0700

    fe: fix authUser to boolean

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,19 +13,20 @@ import useAuthUser from './hooks/useAuthUser.js'
 const App = () => {
 
   const {isLoading, authUser} = useAuthUser();
+  const isAuthenticated = Boolean(authUser);
 
   if (isLoading) return <PageLoader />;
 
   return (
     <div className= 'h-screen' data-theme="night">
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage />  : <Navigate to="/" />} />
-        <Route path="/onboarding" element={authUser ? <OnBoardingPage /> : <Navigate to="/login" />} />
-        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
-        <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/login" />} />
-        <Route path="/notifications" element={authUser ? <NotificationsPage /> : <Navigate to="/login" />} />
+        <Route path="/" element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!isAuthenticated ? <LoginPage />  : <Navigate to="/" />} />
+        <Route path="/onboarding" element={isAuthenticated ? <OnBoardingPage /> : <Navigate to="/login" />} />
+        <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path="/notifications" element={isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" />} />
       </Routes>
 
       <Toaster/>
