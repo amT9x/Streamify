2649e175316a651b40708abc296c428e70e2540e
amT9x <hoangvantung155@gmail.com>
2025-05-27 06:50:47 +0700

    fe: update ui: image preview in onboarding

--- frontend/src/pages/OnBoardingPage.jsx
+++ frontend/src/pages/OnBoardingPage.jsx
@@ -29,7 +29,34 @@ const OnBoardingPage = () => {
   }
 
   return (
-    <div>OnBoardingPage</div>
+    <div className="min-h-screen ba-base-100 flex items-center justify-center p-4">
+      <div className="card bg-base-200 w-full max-w-3xl shadow-xl">
+        <div className="card-body p-6 sm:p-8">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">Complete your profile</h1>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            {/* Profile pic */}
+            <div className="flex flex-col items-center justify-center space-y-4">
+              {/* Image preview */}
+              <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                {formState?.profilePicture ? 
+                (
+                  <img 
+                    src={formState.profilePicture} 
+                    alt="Profile" 
+                    className="w-full h-full object-cover" />
+                ) :
+                (
+                  <div className="flex items-center justify-center h-full">
+                    <CameraIcon className="size-12 text-base-content opacity-40" />
+                  </div>
+                )
+                }
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
