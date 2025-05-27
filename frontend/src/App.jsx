6de25bc1501eea17d79239103015d8c1fd5aa8ba
amT9x <hoangvantung155@gmail.com>
2025-05-27 12:06:55 +0700

    fe: logic chuyen den homepage sau khi dang onboarding thanh cong

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,18 @@ const App = () => {
         />
         <Route path="/signup" element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!isAuthenticated ? <LoginPage />  : <Navigate to="/" />} />
-        <Route path="/onboarding" element={isAuthenticated ? <OnBoardingPage /> : <Navigate to="/login" />} />
+        <Route path="/onboarding" element={isAuthenticated ?
+        ( !isOnBoarded ?
+          (
+            <OnBoardingPage />
+          ) : 
+          (
+            <Navigate to="/" />
+          )
+        ) :
+        (
+          <Navigate to="/login" />
+        )} />
         <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />} />
         <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
         <Route path="/notifications" element={isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" />} />
