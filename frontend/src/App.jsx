28827915a16ecdef956a519574163fa55cb7402e
amT9x <hoangvantung155@gmail.com>
2025-06-05 21:32:31 +0700

    fe: update ui: sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   if (isLoading) return <PageLoader />;
 
   return (
-    <div className= 'h-screen' data-theme="night">
+    <div className= 'h-screen' data-theme="forest">
       <Routes>
         <Route path="/" element=
           {
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,24 @@
+import { Link, useLocation } from "react-router";
+import useAuthUser from "../hooks/useAuthUser";
+import { ShipWheelIcon } from "lucide-react";
+
 const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
+    const { authUser } = useAuthUser();
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    return (
+        <aside className="w-64 bg-base-200 border-r border-base-300 hidden lg:flex flex-col h-screen sticky top-0">
+            <div className="p-5 border-b border-base-300">
+                <Link to="/" className="flex items-center gap-2.5">
+                    <ShipWheelIcon className="size-9 text-primary" />
+                    <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wide">
+                        Streamify
+                    </span>
+                </Link>
+            </div>
+        </aside>
+    )
 }
 
 export default SideBar
