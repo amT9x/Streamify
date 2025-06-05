a089026aabd1fbd26cb274f5b79b2658fb9cbed1
amT9x <hoangvantung155@gmail.com>
2025-06-05 22:08:19 +0700

    fe: add ui sidebar: user profile

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -62,6 +62,29 @@ const SideBar = () => {
                     <span>Notifications</span>
                 </Link>
             </nav>
+
+            {/* User profile */}
+            <div className="p-4 border-t border-base-300 mt-auto">
+                <div className="flex items-center gap-3">
+                    {/* avatar */}
+                    <div className="avatar">
+                        <div className="w-12 rounded-full">
+                            <img
+                                src={authUser?.profilePicture}
+                                alt="Profile"
+                            />
+                        </div>
+                    </div>
+                    
+                    <div className="flex-1">
+                        <p className="text-sm font-semibold">{authUser?.fullName}</p>
+                        <p className="text-xs text-success flex items-center gap-1">
+                            <span className="size-2 rounded-full bg-success inline-block" />
+                            Online
+                        </p>
+                    </div>
+                </div>
+            </div>
         </aside>
     )
 }
