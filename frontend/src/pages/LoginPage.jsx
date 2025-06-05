6eea282557b95fd7e15ab0f7ecb2ab299a2ad6b1
amT9x <hoangvantung155@gmail.com>
2025-06-05 16:18:17 +0700

    fe: complete feature login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,132 @@
-import React from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react'
+import { login } from '../lib/api';
+import { ShipWheelIcon } from 'lucide-react';
+import { Link } from 'react-router';
 
 const LoginPage = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const queryClient = useQueryClient();
+
+  const { mutate: loginMutation, isPending, error } = useMutation({
+    mutationFn: login,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['authUser'] })
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    loginMutation(loginData);
+  }
+
   return (
-    <div>LoginPage</div>
+    <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="forest">
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* Sign up form - left side */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* Logo */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheelIcon className="size-9 text-primary" />
+            <span className="text-2xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wide">
+              Streamify
+            </span>
+          </div>
+
+          {/* Error */}
+          {error && (
+            <div className="alert alert-error mb-4">
+              <span>{error.response.data.message}</span>
+            </div>
+          )}
+
+          {/* Form */}
+          <div className="w-full">
+            <form onSubmit={handleLogin}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Welcome Back</h2>
+                  <p className="text-sm opacity-70">
+                    Sign in to your account
+                  </p>
+                </div>
+                <div className="space-y-3">
+                  {/* Email */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    
+                    <input
+                      type="email"
+                      placeholder="...@email.com"
+                      className="input input-bordered w-full"
+                      value={loginData.email}
+                      onChange={(e) => setLoginData({ ...loginData, email: e.target.value })}
+                      required
+                    />
+                  </div>
+                  {/* Password */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+
+                    <input
+                      type="password"
+                      placeholder="********"
+                      className="input input-bordered w-full"
+                      value={loginData.password}
+                      onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
+                      required
+                    />
+                  </div>
+                  {/* Submit sign up */}
+                  <button className="btn btn-primary w-full" type="submit">
+                    {isPending ? (
+                      <>
+                        <span className="loading loading-spinner loading-xs"></span>
+                        Loading...
+                      </>
+                    ) : (
+                      "Sign In"
+                    )}
+                  </button>
+                  {/* Sign up */}
+                  <div className="text-center mt-4">
+                    <p className="text-sm">
+                      Alreadly have an account? {" "}
+                      <Link to="/signup" className="text-primary hover:underline">
+                        Sign up
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* Sign up form - right side */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/* illustration */}
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img src="../public/signup.png" alt="language connect illustration" className="w-full h-full" />
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">Connect with language parter worldwide</h2>
+              <p className="opacity-70">
+                Practives conversation, make friends, and improve your language skills together
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
