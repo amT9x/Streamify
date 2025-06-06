bf561cf22600ec3ef3bd0b2817c23dd07d1a0f60
amT9x <hoangvantung155@gmail.com>
2025-06-06 10:53:26 +0700

    fe: update ui: theme cua pageloader theo theme web

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,9 +1,11 @@
 import { LoaderIcon } from 'lucide-react';
 import React from 'react'
+import { useThemeStore } from '../stores/useThemeStore';
 
 const PageLoader = () => {
+  const {theme} = useThemeStore();
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center" data-theme={theme}>
       <LoaderIcon className="animate-spin size-12 text-primary" />
     </div>
   );
