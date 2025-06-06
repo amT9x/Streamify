50dfee26a537f2f29c88c71cc6b72e21e5e01aa8
amT9x <hoangvantung155@gmail.com>
2025-06-06 10:50:53 +0700

    fe: logic giu ui khi reload page f5

--- frontend/src/stores/useThemeStore.js
+++ frontend/src/stores/useThemeStore.js
@@ -1,6 +1,9 @@
 import { create } from 'zustand';
 
 export const useThemeStore = create((set) => ({
-  theme: "forest", // default theme
-  setTheme: (theme) => set({ theme }),
+  theme: localStorage.getItem("stremify-theme") || "coffee", // default theme
+  setTheme: (theme) => {
+    localStorage.setItem("stremify-theme", theme);
+    set({ theme });
+  },
 }));
