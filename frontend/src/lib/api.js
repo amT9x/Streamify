e5a0391121e3e068edd3402b584a968183ccc004
amT9x <hoangvantung155@gmail.com>
2025-06-17 21:45:11 +0700

    fe: them cac tinh nang cua homepage: getgetRecommendedUsers, getMyFriends, getOutgoingFriendRequests, sendFriendRequest

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -29,3 +29,23 @@ export const completeOnboarding = async (userData) => {
     const res = await axiosInstance.post('http://localhost:5001/api/auth/onboarding', userData);
     return res.data;
 }
+
+export async function getRecommendedUsers() {
+    const res = await axiosInstance.get('http://localhost:5001/api/users');
+    return res.data;
+}
+
+export async function getMyFriends() {
+    const res = await axiosInstance.get('http://localhost:5001/api/users/friends');
+    return res.data;
+}
+
+export async function getOutgoingFriendRequests() {
+    const res = await axiosInstance.get('http://localhost:5001/api/users/outgoing-friend-requests');
+    return res.data;
+}
+
+export async function sendFriendRequest(userId) {
+    const res = await axiosInstance.post(`http://localhost:5001/api/users/friend-request/${userId}`);
+    return res.data;
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { getMyFriends, getOutgoingFriendRequests, getRecommendedUsers, sendFriendRequest } from "../lib/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Link } from "react-router";
+import { UserIcon } from "lucide-react";
 
 const HomePage = () => {
+  const queryClient = useQueryClient()
+  const [outgoingRequestsIds, setOutgoingRequestsIds] = useState(new Set())
+
+  const {data: myFriends, isLoading: loadingMyFriends} = useQuery({
+    queryKey: ["myFriends"],
+    queryFn: getMyFriends
+  })
+
+  const {data: recommendedUsers, isLoading: loadingRecommendedUsers} = useQuery({
+    queryKey: ["recommendedUsers"],
+    queryFn: getRecommendedUsers
+  })
+
+  const {data: outgoingFriendRequests} = useQuery({
+    queryKey: ["outgoingFriendRequests"],
+    queryFn: getOutgoingFriendRequests
+  })
+
+  const {mutate: sendRequestMutation, isPending} = useMutation({
+    mutationFn: sendFriendRequest,
+    onSuccess: () => queryClient.invalidateQueries(["outgoingFriendRequests"]),
+  })
+
+  useEffect(() => {
+    const outGoingIds = new Set()
+    
+    if (outgoingFriendRequests && outgoingFriendRequests.length > 0) {
+      outgoingFriendRequests.forEach(request => {
+        outGoingIds.add(request.id)
+      })
+    }
+  }, [outgoingFriendRequests])
+
   return (
-    <div>HomePage</div>
+    <div className="p-4 sm:p-6 lg:p-8">
+      <div className="container mx-auto space-y-10">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
+          <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">Your Friends</h2>
+          <Link to="/notifications" className="btn btn-outline btn-sm">
+            <UserIcon className="size-4 ms-2" />
+            Friend Requests
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
